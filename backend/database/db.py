ff7f4684853eb37ed62e96e479437938fa51fe12
Gustavo Figueiredo <gustavofsilva10@hotmail.com>
2025-01-15 00:29:26 -0300

    feat: implementa tabelas lancamentos e terceiros

--- backend/database/db.py
+++ backend/database/db.py
@@ -12,20 +12,47 @@ def banco_inicializador():
 
     # Tabela usuarios
     cursor.execute(''' 
-    CREATE TABLE IF NOT EXISTS users (
+    CREATE TABLE IF NOT EXISTS usuarios (
                 id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
                 id_arquivos INTEGER,
-                usuario TEXT NOT NULL,
+                nome TEXT NOT NULL,
+                email TEXT NOT NULL,
+                login TEXT NOT NULL,
+                senha INTERGER NOT NULL,
                 admin INTEGER NOT NULL); 
                 ''')
+    # Tabela Terceiros
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS terceiros (
+                id_terceiros INTEGER PRIMARY KEY AUTOINCREMENT,
+                razao TEXT NOT NULL,
+                nome_fantasia TEXT NOT NULL,
+                cpf_cpnj TEXT NOT NULL,
+                email TEXT NOT NULL,
+                telefone INTEGER,
+                categoria TEXT NOT NULL);''')
     
+    # Tabela Lançamentos
+    #OBS.: Explicar um pocuo melhor sobre quem o emitente vai referenciar
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS lancamentos (
+                id_lancamentos INTEGER PRIMARY KEY AUTOINCREMENT,
+                data TEXT NOT NULL,
+                valor_total REAL NOT NULL,
+                valor_pago REAL NOT NULL,
+                operacao TEXT NOT NULL,
+                emitente INTEGER,
+                tipo_operacao TEXT NOT NULL,
+                descricao TEXT);''')
+
     # Tabela arquivos
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS files (
+    CREATE TABLE IF NOT EXISTS arquivos (
                 id_arquivo INTEGER NOT NULL,
                 arquivo BLOB NOT NULL,
-                FOREIGN KEY (id_arquivo) REFERENCES users(id_arquivos));'''
-                   )
+                FOREIGN KEY (id_arquivo) REFERENCES users(id_arquivos));''')
+    
     conn.close()
 
 
