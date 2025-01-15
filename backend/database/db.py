c63841b9dbce63999ec70a080d0ff1829531acd0
Gustavo Figueiredo <gustavofsilva10@hotmail.com>
2025-01-15 00:36:16 -0300

    feat: adiciona funcoes de debug a funcao banco_inicializador

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,61 +1,61 @@
 import sqlite3 as sq3
 
 
-# Add Try Except depois
 def banco_inicializador():
     # Inicialização
     conn = sq3.connect('backend/database/database.db')
     cursor = conn.cursor()
     conn.execute('PRAGMA foreign_keys = ON;')       # Habilita chaves estrangeiras
 
-    # obs.: SQLite3 não possui datatype booleano, melhor fazer com 0 ou 1
+    try:
+        # Tabela usuarios
+        cursor.execute(''' 
+        CREATE TABLE IF NOT EXISTS usuarios (
+                    id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
+                    id_arquivos INTEGER,
+                    nome TEXT NOT NULL,
+                    email TEXT NOT NULL,
+                    login TEXT NOT NULL,
+                    senha INTERGER NOT NULL,
+                    admin INTEGER NOT NULL); ''')
+        
+        # Tabela Terceiros
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS terceiros (
+                    id_terceiros INTEGER PRIMARY KEY AUTOINCREMENT,
+                    razao TEXT NOT NULL,
+                    nome_fantasia TEXT NOT NULL,
+                    cpf_cpnj TEXT NOT NULL,
+                    email TEXT NOT NULL,
+                    telefone INTEGER,
+                    categoria TEXT NOT NULL);''')
 
-    # Tabela usuarios
-    cursor.execute(''' 
-    CREATE TABLE IF NOT EXISTS usuarios (
-                id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
-                id_arquivos INTEGER,
-                nome TEXT NOT NULL,
-                email TEXT NOT NULL,
-                login TEXT NOT NULL,
-                senha INTERGER NOT NULL,
-                admin INTEGER NOT NULL); 
-                ''')
-    # Tabela Terceiros
+        # Tabela Lançamentos
+        # OBS.: Explicar um pocuo melhor sobre quem o emitente vai referenciar
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS lancamentos (
+                    id_lancamentos INTEGER PRIMARY KEY AUTOINCREMENT,
+                    data TEXT NOT NULL,
+                    valor_total REAL NOT NULL,
+                    valor_pago REAL NOT NULL,
+                    operacao TEXT NOT NULL,
+                    emitente INTEGER,
+                    tipo_operacao TEXT NOT NULL,
+                    descricao TEXT);''')
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS terceiros (
-                id_terceiros INTEGER PRIMARY KEY AUTOINCREMENT,
-                razao TEXT NOT NULL,
-                nome_fantasia TEXT NOT NULL,
-                cpf_cpnj TEXT NOT NULL,
-                email TEXT NOT NULL,
-                telefone INTEGER,
-                categoria TEXT NOT NULL);''')
-    
-    # Tabela Lançamentos
-    #OBS.: Explicar um pocuo melhor sobre quem o emitente vai referenciar
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS lancamentos (
-                id_lancamentos INTEGER PRIMARY KEY AUTOINCREMENT,
-                data TEXT NOT NULL,
-                valor_total REAL NOT NULL,
-                valor_pago REAL NOT NULL,
-                operacao TEXT NOT NULL,
-                emitente INTEGER,
-                tipo_operacao TEXT NOT NULL,
-                descricao TEXT);''')
+        # Tabela arquivos
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS arquivos (
+                    id_arquivo INTEGER NOT NULL,
+                    arquivo BLOB NOT NULL,
+                    FOREIGN KEY (id_arquivo) REFERENCES users(id_arquivos));''')
 
-    # Tabela arquivos
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS arquivos (
-                id_arquivo INTEGER NOT NULL,
-                arquivo BLOB NOT NULL,
-                FOREIGN KEY (id_arquivo) REFERENCES users(id_arquivos));''')
+    except Exception as e:
+        print(f'Log: falha ao gerar tabela usuarios: {e}')
+        conn.close()
+        return f'Log: falha ao gerar tabela usuarios: {e}'
     
     conn.close()
-
-
 
 # Só para testes, chamar função na inicialização
 if __name__ == '__main__':
