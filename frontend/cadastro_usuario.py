18765d3508c376076e4b1d9d2fea69d11aa1b47b
Tecla Fernandes Oliveira <115437392+TeclaFernandes@users.noreply.github.com>
2025-01-15 13:50:40 -0300

    feat: Tela de cadastro de usuarios

--- frontend/cadastro_usuario.py
+++ frontend/cadastro_usuario.py
@@ -8,6 +8,70 @@ def Cadastro_Usuario(instance):
                          font=(instance.font_2, 15, 'bold'), bg=instance.color_2, fg=instance.color_3)
     select_label.place(x=40, y=20)
 
+    # Caixa de entrada para "Nome completo"
+    name_label = Label(instance.frame_1, text="Nome completo:",
+        font=(instance.font_4, 12), bg=instance.color_2, fg=instance.color_3)
+    name_label.place(x=70, y=100)
+
+    name_entry = Entry(instance.frame_1, width=40, font=(instance.font_4, 14))
+    name_entry.place(x=200, y=100)
+
+    # Caixa de entrada para "Email"
+    email_label = Label(instance.frame_1, text="Email:",
+        font=(instance.font_4, 12), bg=instance.color_2, fg=instance.color_3)
+    email_label.place(x=138, y=150)
+
+    email_entry = Entry(instance.frame_1, width=40, font=(instance.font_4, 14))
+    email_entry.place(x=200, y=150)
+
+    # Caixa de entrada para "Data de Nascimento"
+    name_label = Label(instance.frame_1, text="Data de nascimento:",
+        font=(instance.font_4, 12), bg=instance.color_2, fg=instance.color_3)
+    name_label.place(x=40, y=200)
+
+    name_entry = Entry(instance.frame_1, width=13, font=(instance.font_4, 14))
+    name_entry.place(x=200, y=200)
+
+    # Caixa de entrada para "Perfil"
+    email_label = Label(instance.frame_1, text="Perfil:",
+        font=(instance.font_4, 12), bg=instance.color_2, fg=instance.color_3)
+    email_label.place(x=142, y=250)
+
+    email_entry = Entry(instance.frame_1, width=13, font=(instance.font_4, 14))
+    email_entry.place(x=200, y=250)
+
+    # Caixa de entrada para "Login"
+    name_label = Label(instance.frame_1, text="Login:",
+        font=(instance.font_4, 12), bg=instance.color_2, fg=instance.color_3)
+    name_label.place(x=139, y=300)
+
+    name_entry = Entry(instance.frame_1, width=20, font=(instance.font_4, 14))
+    name_entry.place(x=200, y=300)
+
+    # Caixa de entrada para "Senha"
+    email_label = Label(instance.frame_1, text="Senha:",
+        font=(instance.font_4, 12), bg=instance.color_2, fg=instance.color_3)
+    email_label.place(x=135, y=350)
+
+    email_entry = Entry(instance.frame_1, width=13, font=(instance.font_4, 14))
+    email_entry.place(x=200, y=350)
+
+    # Caixa de entrada para "Repetir senha"
+    name_label = Label(instance.frame_1, text="Repetir senha:",
+        font=(instance.font_4, 12), bg=instance.color_2, fg=instance.color_3)
+    name_label.place(x=82, y=400)
+
+    name_entry = Entry(instance.frame_1, width=13, font=(instance.font_4, 14))
+    name_entry.place(x=200, y=400)
+
+    # Caixa de entrada para "Código"
+    email_label = Label(instance.frame_1, text="Código:",
+        font=(instance.font_4, 12), bg=instance.color_2, fg=instance.color_3)
+    email_label.place(x=128, y=450)
+
+    email_entry = Entry(instance.frame_1, width=10, font=(instance.font_4, 14))
+    email_entry.place(x=200, y=450)
+
      # Botão Salvar
     merge_button = Button(instance.frame_1, text="Salvar",
                           font=(instance.font_3, 15, 'bold'), bg=instance.color_4, fg=instance.color_1,
