25de6d16ac2d5a05138fbb1b2f312b018aef18fc
Lucas Oliveira <oliveira.lucas62@aluno.ifce.edu.br>
2025-02-04 19:10:01 -0300

    Chore: Contrução de tela emissão

--- app/tela_emissao.py
+++ app/tela_emissao.py
@@ -7,3 +7,74 @@ def Tela_Emissao(instance):
     select_label = Label(instance.frame_1, text="Bem-Vindo a tela de Emissão",
                          font=(instance.font_2, 15, 'bold'), bg=instance.color_2, fg=instance.color_3)
     select_label.place(x=40, y=20)
+
+    #Label e Entrada CFOP
+    label_CFOP = Label(instance.frame_1, text="CFOP:",bg=instance.color_2,fg=instance.color_3,
+                       font=("Arial", 12))
+    label_CFOP.place(x=250,y=120)
+    entry_CFOP = Entry(instance.frame_1,font=("Arial",12),width=20,
+                       foreground="white")
+    entry_CFOP.place(x=310,y=120)
+
+    #Label e Entrada Produto/Serviço
+    label_produto = Label(instance.frame_1,text="Produto/Serviço:",bg=instance.color_2,fg=instance.color_3,
+                          font=("Arial", 12))
+    label_produto.place(x=180,y=160)
+
+
+
+    #Label e Entrada Valor produto/serviço
+    label_valor_produto = Label(instance.frame_1,text="Valor produto/serviço:", bg=instance.color_2,fg=instance.color_3,
+                                font=("Arial", 12))
+    label_valor_produto.place(x= 144,y=200)
+
+
+
+    #Label e Entrada Tributação ICMS
+    label_tributacao = Label(instance.frame_1, text="Tributação ICMS:", bg=instance.color_2,fg=instance.color_3,
+                             font=("Arial", 12))
+    label_tributacao.place(x=175,y=240)
+
+
+
+    #Label e Entrada Valor ICMS
+    label_valor_icm = Label(instance.frame_1, text="Valor ICMS:", bg=instance.color_2,fg=instance.color_3,
+                            font=("Arial", 12))
+    label_valor_icm.place(x=210,y=280)
+
+
+
+    #Label e Entrada Valor IPI
+    label_valor_ipi = Label(instance.frame_1, text="Valor IPI:", bg=instance.color_2,fg=instance.color_3,
+                            font=("Arial", 12))
+    label_valor_ipi.place(x=230,y=320)
+
+
+
+    #Label e Entrada Valor PIS
+    label_valor_pis = Label(instance.frame_1, text="Valor PIS:", bg=instance.color_2,fg=instance.color_3,
+                            font=("Arial", 12))
+    label_valor_pis. place(x=222, y=360)
+
+
+
+    #Label e Entrada Descontos
+    label_descontos = Label(instance.frame_1, text="Descontos:", bg=instance.color_2,fg=instance.color_3,
+                            font=("Arial", 12))
+    label_descontos.place(x=215, y=400)
+
+
+
+    #Label e Entrada Observações
+    label_observacoes = Label(instance.frame_1, text="Observações:", bg=instance.color_2,fg=instance.color_3,
+                              font=("Arial", 12))
+    label_observacoes.place(x=196,y=440)
+
+
+
+
+
+
+
+
+    
