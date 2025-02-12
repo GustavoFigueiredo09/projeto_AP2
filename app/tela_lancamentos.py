abb679716006534d5c6cf6f6405c0f7f80f9807a
Lucas Oliveira <oliveira.lucas62@aluno.ifce.edu.br>
2025-02-12 15:45:30 -0300

    chore: ajustes nas entradas e labels da tela lançamento

--- app/tela_lancamentos.py
+++ app/tela_lancamentos.py
@@ -45,65 +45,85 @@ def Tela_Lancamentos(root):
         cb_banco.delete(0, tk.END)
         desc_entry.delete(0,tk.END)
         
-    #def excluir_lancamento():
-    # 
-    # 
-    # 
+    def excluir_lancamento():
+        codigo = entry_cod.get().strip()
+
+        if not codigo:
+            messagebox.showwarning("Aviso", "Digite um código antes de excluir!")
+            return
+
+        lancamento = Lancamento()  # Criando um objeto da classe Lancamento
+        if lancamento.delete(codigo):  # Chama diretamente a função delete
+            messagebox.showinfo("Sucesso", "Lançamento excluído com sucesso!")
+            limpar_tudo()
+        else:
+            messagebox.showwarning("Aviso", "Nenhum lançamento encontrado com esse código!")
+
+
+   
     #Labels & Entradas 
-    lbl_data = tk.Label(root.frame_1, text="Data:", bg="#FDFDE3", font=("Helvetica",12,"bold"))
+    label_lancamento = Label(root.frame_1, text="Tela de Lançamentos", font=("Arial", 15, "bold"),bg="#FDFDE3")
+    label_lancamento.place(x=600,y=10)
+
+    label_cod = Label(root.frame_1, text="Código:", bg="#FDFDE3", font=("Arial", 12))
+    label_cod.place(x=285, y=110)
+    entry_cod = Entry(root.frame_1,font=("Arial", 12),width= 72 )
+    entry_cod.place(x=350, y=110)
+
+    #LABEL E ENTRADA DATA
+    lbl_data = tk.Label(root.frame_1, text="Data:", bg="#FDFDE3", font=("Arial",12))
     lbl_data.place(x=304,y=160)
-    data_entry = ctk.CTkEntry(root.frame_1, font=("Arial", 12), width= 200,
-                              fg_color="white",text_color="black")
+    data_entry = Entry(root.frame_1, font=("Arial", 12), width= 20)
     data_entry.place(x=350, y=160)
 
-    lbl_valor = tk.Label(root.frame_1, text="Valor total da nota:", bg="#FDFDE3", font=("Helvetica",12,"bold"))
-    lbl_valor.place(x=202,y=210)
-    nota_entry = ctk.CTkEntry(root.frame_1, font=("Arial", 12),width=200, 
-                             fg_color="white", text_color="black")
+    #LABEL E ENTRADA VALOR
+    lbl_valor = tk.Label(root.frame_1, text="Valor total da nota:", bg="#FDFDE3", font=("Arial",12))
+    lbl_valor.place(x=210,y=210)
+    nota_entry = Entry(root.frame_1, font=("Arial", 12),width=20)
     nota_entry.place(x=350,y=210)
 
-    lbl_produto = tk.Label(root.frame_1, text="Valor total dos produtos:", bg="#FDFDE3", font=("Helvetica",12,"bold"))
-    lbl_produto.place(x=156,y=260)
-    produto_entry = ctk.CTkEntry(root.frame_1, font=("Arial", 12),width=200, 
-                                 fg_color="white", text_color="black")
+    #LABEL E ENTRADA PRODUTO
+    lbl_produto = tk.Label(root.frame_1, text="Valor total dos produtos:", bg="#FDFDE3", font=("Arial",12))
+    lbl_produto.place(x=170,y=260)
+    produto_entry = Entry(root.frame_1, font=("Arial", 12),width=20)
     produto_entry.place(x=350, y=260)
 
-    lbl_impostos = tk.Label(root.frame_1, text="Valor dos impostos:", bg="#FDFDE3", font=("Helvetica",12,"bold"))
-    lbl_impostos.place(x=192,y=310)
-    imp_entry = ctk.CTkEntry(root.frame_1, font=("Arial", 12),width=200, 
-                             fg_color="white", text_color="black")
+    #LABEL E ENTRADA IMPOSTOS
+    lbl_impostos = tk.Label(root.frame_1, text="Valor dos impostos:", bg="#FDFDE3", font=("Arial",12))
+    lbl_impostos.place(x=200,y=310)
+    imp_entry = Entry(root.frame_1, font=("Arial", 12),width=20)
     imp_entry.place(x=350, y=310)
 
-    lbl_operacao= tk.Label(root.frame_1, text="Operação:", bg="#FDFDE3", font=("Helvetica",12,"bold"))
+    #LABEL E ENTRADA OPERAÇÃO
+    lbl_operacao= tk.Label(root.frame_1, text="Operação:", bg="#FDFDE3", font=("Arial",12))
     lbl_operacao.place(x=267,y=360)
-    oper_entry = ctk.CTkEntry(root.frame_1, font=("Arial", 12), width=200, 
-                             fg_color="white", text_color="black")
+    oper_entry = Entry(root.frame_1, font=("Arial", 12), width=20)
     oper_entry.place(x=350,y=360)
 
-    lbl_emitente = tk.Label(root.frame_1, text="Emitente:", bg="#FDFDE3", font=("Helvetica",12,"bold"))
-    lbl_emitente.place(x=272,y=410)
-    emt_entry = ctk.CTkEntry(root.frame_1, font=("Arial", 12), width=200, 
-                             fg_color="white", text_color="black")
+    #LABEL E ENTRADA EMITENTE
+    lbl_emitente = tk.Label(root.frame_1, text="Emitente:", bg="#FDFDE3", font=("Arial",12))
+    lbl_emitente.place(x=274,y=410)
+    emt_entry = Entry(root.frame_1, font=("Arial", 12), width=20)
     emt_entry.place(x=350,y=410)
 
-    lbl_tipo = tk.Label(root.frame_1, text="Tipo de operação:", bg="#FDFDE3", font=("Helvetica",12,"bold"))
-    lbl_tipo.place(x=664,y=160)
-
+    #LABEL TIPO DE OPERÇÃO E COMBOBOX OPÇÕES
+    lbl_tipo = tk.Label(root.frame_1, text="Tipo de operação:", bg="#FDFDE3", font=("Arial",12))
+    lbl_tipo.place(x=668,y=160)
     opcoes = ["Entrada", "Saída"]
-    cb_tipo = ttk.Combobox(root.frame_1, values=opcoes, width=40, style="Custom.TCombobox")
+    cb_tipo = ttk.Combobox(root.frame_1, values=opcoes, width=27, style="Custom.TCombobox")
     cb_tipo.place(x=810,y=160)
 
-    lbl_banco = tk.Label(root.frame_1, text="Banco:", bg="#FDFDE3", font=("Helvetica",12,"bold"))
-    lbl_banco.place(x=720,y=210)
-
+    #LABEL BANCO E COMBOBOX OPÇÕES DE BANCO
+    lbl_banco = tk.Label(root.frame_1, text="Banco:", bg="#FDFDE3", font=("Arial",12))
+    lbl_banco.place(x=748,y=210)
     opcoes_banco = ["Nubank", "Pic-Pay", "Inter", "Itaú", "Mercado Pago", "Caixa", "Will"]
-    cb_banco = ttk.Combobox(root.frame_1, values=opcoes_banco, width=40)
+    cb_banco = ttk.Combobox(root.frame_1, values=opcoes_banco, width=27)
     cb_banco.place(x=810,y=210)
 
-    lbl_descricao = tk.Label(root.frame_1, text="Descrição:", bg="#FDFDE3", font=("Helvetica",12,"bold"))
+    # LABEL E ENTRADA DESCRIÇÃO
+    lbl_descricao = tk.Label(root.frame_1, text="Descrição:", bg="#FDFDE3", font=("Arial",12))
     lbl_descricao.place(x=720,y=260)
-    desc_entry = ctk.CTkEntry(root.frame_1, font=("Arial", 12), width=200, 
-                             fg_color="white", text_color="black")
+    desc_entry = Entry(root.frame_1, font=("Arial", 12), width=20)
     desc_entry.place(x=810,y=260)
 
     # Botões
